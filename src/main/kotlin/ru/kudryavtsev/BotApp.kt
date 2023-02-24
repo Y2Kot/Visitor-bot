4826e98cc9f36e25a908b5c4de39721cc9631406
mkudryavtsev <maxim.kudryavtsev@qoollo.com>
2023-02-24 20:28:20 +0300

    adding logger implementation

--- src/main/kotlin/ru/kudryavtsev/BotApp.kt
+++ src/main/kotlin/ru/kudryavtsev/BotApp.kt
@@ -1,7 +1,9 @@
 package ru.kudryavtsev
 
+import com.qoollo.common.model.FilePolicy
 import com.qoollo.logback.LogbackLogger
 import com.qoollo.logger.QoolloLogger
+import com.qoollo.logger.logi
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.awaitCancellation
@@ -13,14 +15,23 @@ import org.jetbrains.exposed.sql.addLogger
 import org.jetbrains.exposed.sql.transactions.TransactionManager
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.koin.core.context.startKoin
+import ru.kudryavtsev.datasource.local.entity.Students
+import ru.kudryavtsev.datasource.local.entity.Visits
 import ru.kudryavtsev.domain.BotController
 import ru.kudryavtsev.domain.di.domainModule
 import ru.kudryavtsev.domain.di.remoteModule
+import ru.kudryavtsev.domain.model.AppContext
 import java.sql.Connection
 
 suspend fun main() {
-    initializeDb()
-    initializeLogger()
+    val currentContext = AppContext.getEnvironment()
+    initializeLogger(currentContext)
+    logi(tag = "BotApp") {
+        "selected context: ${currentContext.javaClass.simpleName}\n" +
+                "volume path: ${currentContext.volumePath}\n" +
+                "db path: ${currentContext.dbPath}"
+    }
+    initializeDb(currentContext)
     startKoin {
         modules(domainModule, remoteModule)
     }
@@ -30,20 +41,28 @@ suspend fun main() {
     awaitCancellation()
 }
 
-private fun initializeDb() {
-    Database.connect("jdbc:sqlite:/data/data.db", "org.sqlite.JDBC")
+private fun initializeDb(context: AppContext) {
+    Database.connect("jdbc:sqlite:${context.dbPath}", "org.sqlite.JDBC")
     TransactionManager.manager.defaultIsolationLevel = Connection.TRANSACTION_SERIALIZABLE
     transaction {
         addLogger(StdOutSqlLogger)
-        SchemaUtils.create(ru.kudryavtsev.datasource.local.entity.Visits)
-        SchemaUtils.create(ru.kudryavtsev.datasource.local.entity.Students)
+        SchemaUtils.create(Visits)
+        SchemaUtils.create(Students)
     }
 }
 
-private fun initializeLogger() {
+private fun initializeLogger(context: AppContext) {
     QoolloLogger {
         val logbackKey = "logback"
+        val filePolicy = FilePolicy(
+            fileName = "BotLoggerFiles",
+            filesPath = context.volumePath,
+            fileSize = 1024 * 1024 * 6,
+            filesCount = 5,
+            append = true
+        )
         registerLogger(logbackKey, LogbackLogger(logbackKey))
         enableConsoleLogging(logbackKey)
+        enableFileLogging(logbackKey, filePolicy)
     }
 }
