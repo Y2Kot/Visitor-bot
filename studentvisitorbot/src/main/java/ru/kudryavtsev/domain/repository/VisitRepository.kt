ea03b52d2500436f4ca7aff400cea84bab0e2b8b
mkudryavtsev <kudryavtsevma@yandex.ru>
2023-05-14 22:00:23 +0300

    adding validation to sending visits

--- studentvisitorbot/src/main/java/ru/kudryavtsev/domain/repository/VisitRepository.kt
+++ studentvisitorbot/src/main/java/ru/kudryavtsev/domain/repository/VisitRepository.kt
@@ -3,11 +3,17 @@ package ru.kudryavtsev.domain.repository
 import ru.kudryavtsev.datasource.local.mappers.toDomain
 import ru.kudryavtsev.datasource.local.services.VisitDaoService
 import ru.kudryavtsev.domain.model.Visit
+import java.time.LocalDate
 
 class VisitRepository(private val service: VisitDaoService) {
     fun registerVisit(visit: Visit) {
         service.create(visit)
     }
 
-    fun getAllVisits(): List<Visit> = service.readAll().map { it.toDomain() }
+    fun getVisitByDate(date: LocalDate): List<Visit> = service
+        .readReadByDate(date)
+        .map { it.toDomain() }
+    fun getAllVisits(): List<Visit> = service
+        .readAll()
+        .map { it.toDomain() }
 }
